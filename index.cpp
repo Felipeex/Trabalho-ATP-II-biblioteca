ecaaf9d4aa9bf224d5a6c9cdd276392f7dbe0208
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2024-11-19 00:54:43 -0300

    Apagando linha ao cadastrar em pessoa.

--- index.cpp
+++ index.cpp
@@ -296,7 +296,6 @@ void CadastrarPessoa() {
     if (PonteiroPessoaArquivo != NULL) {
       fseek(PonteiroPessoaArquivo, 0, 2);
       ultimoId = ftell(PonteiroPessoaArquivo) / sizeof(Pessoa);
-      printf("%d %d %d", ftell(PonteiroPessoaArquivo), sizeof(Pessoa), ftell(PonteiroPessoaArquivo) / sizeof(Pessoa));
       novaPessoa.id = ultimoId + 1;
 
       printf(RED "\nDados do nova pessoa de número " NORMAL "#%d\n", novaPessoa.id);
@@ -319,6 +318,7 @@ void CadastrarPessoa() {
         fwrite(&novaPessoa, sizeof(Pessoa), 1, PonteiroPessoaArquivo);
       }
 
+      limparLinhas(5);
     } else printf("\n Não foi possivel abrir o arquivo pessoa.");
   } while(PonteiroPessoaArquivo != NULL && strlen(novaPessoa.nome) >= 1);
   fclose(PonteiroPessoaArquivo);
